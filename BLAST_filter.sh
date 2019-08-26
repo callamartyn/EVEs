816310dae5fceae3373d643084c689abf082a88d
Calla Martyn <callamartyn@gmail.com>
2019-08-26 11:42:23 -0700

    updating local git

--- BLAST_filter.sh
+++ BLAST_filter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cut -d, -f9,10 ~/chou_lab/EVE/test_filtered.csv
-cut -d, -f13 ~/chou_lab/EVE/test_filtered.csv > test_names.csv
+cut -d, -f13 ~/chou_lab/EVE/test_filtered.csv
+> test_names.csv
