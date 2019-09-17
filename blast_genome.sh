96ab0d9398485b680931a066430cb09701eabc51
Calla <callamartyn@gmail.com>
2019-09-17 10:32:20 -0700

    changing blast outfmt

--- blast_genome.sh
+++ blast_genome.sh
@@ -1,4 +1,6 @@
 #!/bin/env bash
 $HOME/programs/ncbi-blast-2.9.0+/bin/blastx -query $HOME/EVE/data/I_scapularis/GCA_000208615.1_JCVI_ISG_i3_1.0_genomic.fna \
--db $HOME/db/viral/refseq/viral.protein.all.faa -outfmt '5' -out $HOME/EVE/out/Iscap_blastx_vprot_1e-03.out -evalue 1e-03 \
+-db $HOME/db/viral/refseq/viral.1.protein.faa -outfmt '6' -out $HOME/EVE/out/Iscap_blastx_vprot_1e-03.tsv -evalue 1e-03 \
 -num_threads $NSLOTS
+
+qstat -j $JOB_ID
